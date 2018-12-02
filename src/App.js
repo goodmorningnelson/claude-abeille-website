1f8631cc80e6108663bf091ecb57cc93fbfebc6c
goodmorningnelson <mtr.ruehr@gmail.com>
2018-12-02 11:40:22 +0100

    netlify test n2t

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn how to netlify baby !
+            Learn how to netlify !
           </a>
         </header>
       </div>
